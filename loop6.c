6c9da23f52b519ad8ebbb082ef4960232544de7e
Shubham22012004 <145416455+Shubham22012004@users.noreply.github.com>
2023-09-25 17:41:35 +0530

    Add files via upload

--- loop6.c
+++ loop6.c
@@ -1,3 +1,4 @@
+
 #include<stdio.h>
 int main()
 {
@@ -5,22 +6,15 @@ int main()
     for(i=1; i<=4; i++)
     {
         for(k=4; k>=i; k--)
-        {
             printf(" ");
-        }
-        
-        for(j=i; j<=2*i-1; j++)
-        {
+        for(j=i; j>=1; j--)
             printf("%d", j);
-        }
-        if(j>i+1)
-        {
-            for(l=j-1; l>i; l--)
-            {
-                printf("%d", l-1);
-            }
-        } 
-        printf("\n");
+        
+        if(j<i)
+            for(l=j+2; l<=i; l++)
+                printf("%d", l);
+            
+         printf("\n");
     }
     return 0;
 }
